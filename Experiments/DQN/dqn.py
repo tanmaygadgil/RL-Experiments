60445cb80de21bddab62c88533e554937528e432
Tanmay <tanmaygadgil96@gmail.com>
2022-05-25 16:08:15 +0400

    adding dqn code

---
+++ Experiments/DQN/dqn.py
@@ -0,0 +1,30 @@
+from msilib.schema import Class
+import numpy as np
+import tensorflow as tf
+
+
+
+## Hardcoded values
+
+
+
+## Experience replay class
+
+class ExperienceReplay():
+    def __init__(self, size) -> None:
+        self.size = size
+        
+    def learn(self):
+        pass
+    
+    def sample(self):
+        pass
+    
+    
+def dqn():
+    pass
+
+if __name__ == "__main__":
+    
+    pass
+        
